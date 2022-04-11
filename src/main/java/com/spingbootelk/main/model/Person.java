c2bc69fdfc0203641f05300d5a0c3a45b6d95d6a
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2022-04-11 21:56:50 +0300

    Task 2 : Define Person Entity

---
+++ src/main/java/com/spingbootelk/main/model/Person.java
@@ -0,0 +1,38 @@
+package com.spingbootelk.main.model;
+
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Builder
+@Entity
+@Table(name = "person")
+@NoArgsConstructor
+public class Person implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "birth_date")
+    private LocalDate birthDate;
+
+    @Column(name = "password")
+    private String password;
+
+}
