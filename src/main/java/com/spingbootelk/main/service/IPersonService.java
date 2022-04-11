68834e5d4171bfc8ff8a3f111a8ad978e8d49e65
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2022-04-11 22:09:25 +0300

    Task 4 : Define IPersonService

---
+++ src/main/java/com/spingbootelk/main/service/IPersonService.java
@@ -0,0 +1,15 @@
+package com.spingbootelk.main.service;
+
+import com.spingbootelk.main.model.Person;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IPersonService {
+
+    public List<Person> allPersons();
+    public Person savePerson(Person person);
+    public Optional<Person> getPersonById(Long id);
+    public Person updatePerson(Person person);
+    public Person deletePersonById(Long id);
+}
